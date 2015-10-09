d50d45ca46cb36042a6f339a8beea7bfa351f43e
mazhey <mazhey@live.com>
2015-10-09 16:46:57 -0400

    need to be done

---
+++ CompositionResponse/src/Education.java
@@ -0,0 +1,33 @@
+
+public class Education {
+	protected static String[] schools =
+		{							  "school1", 
+									  "school2", 
+									  "school3", 
+									  "school4",
+									  "school5",
+									  "school6",
+									  "school7",
+									  "school8",
+									  "school9",
+									  "school10",
+									  "school11",
+									  "school12",
+									  "school13",
+									  "school14",
+									  "school15",
+									  "school16",
+									  "school17",
+									  "school18",
+									  "school19",
+									  "school20" 
+	};
+	
+	 @Override
+	    public String toString()
+	    {   int i = 20;
+	        for (i );
+	    }
+
+	
+}
