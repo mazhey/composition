9a8fb90e359c0ff58a9b18141f1b958e02586603
mazhey <mazhey@live.com>
2015-10-13 14:02:01 -0400

    update

--- CompositionResponse/src/Education.java
+++ CompositionResponse/src/Education.java
@@ -1,33 +1,29 @@
-
 public class Education {
-	protected static String[] schools =
-		{							  "school1", 
-									  "school2", 
-									  "school3", 
-									  "school4",
-									  "school5",
-									  "school6",
-									  "school7",
-									  "school8",
-									  "school9",
-									  "school10",
-									  "school11",
-									  "school12",
-									  "school13",
-									  "school14",
-									  "school15",
-									  "school16",
-									  "school17",
-									  "school18",
-									  "school19",
-									  "school20" 
-	};
-	
-	 @Override
-	    public String toString()
-	    {   int i = 20;
-	        for (i );
-	    }
+	private String[] education;
 
-	
+	public String[] getEducation() {
+		return education;
+	}
+
+	public void setEducation(String[] education) {
+		this.education = education;
+	}
+
+	protected static String[] schools = { "school1", "school2", "school3",
+			"school4", "school5", "school6", "school7", "school8", "school9",
+			"school10", "school11", "school12", "school13", "school14",
+			"school15", "school16", "school17", "school18", "school19",
+			"school20" };
+
+	@Override
+	public String toString() {
+		String records = "";
+		for (int i = 19; i > 8; i--) {
+			records = schools[i];
+			System.out.println(records);
+		}
+		return "";
+
+	}
+
 }
--- CompositionResponse/src/Person.java
+++ CompositionResponse/src/Person.java
@@ -2,18 +2,24 @@
 public class Person {
 	//composition has-a relationship
     private Job job;
-    private Education education ;
+    public Education education ;
     public Person(){
         this.job=new Job();
+       this.education = new Education();
         job.setSalary(1000L);
+//      education.toString();
     }
     public long getSalary() {
         return job.getSalary();
     }
 
-   
+    public String toString(){
+    	System.out.println();
+    	System.out.println(education.toString());
+    	return "";
+    }
    
 		
-		
+    
 	
 }
